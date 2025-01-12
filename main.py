3229bcfdbe47e30515ba6ea76e58ec49a5b262b4
JordanKisiel <jordan.kisiel@gmail.com>
2025-01-12 14:04:35 -0500

    trying out some dynamic behavior

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ from random_walk import *
 
 def main():
    canvas = Canvas(800, 600) 
-   walker = Walker(canvas)
-   random_walk = Random_Walk(walker)
-   random_walk.set_stride(1)
-   down_right_walk = Weighted_Walk(walker)
-   down_right_walk.set_weights([0.1, 0.4, 0.4, 0.1])
-   walker.set_strategy(random_walk)
+   walker = Walker(canvas, 1)
+   walker.set_stride(2)
+   walk = Weighted_Walk(walker)
+   walk.set_weights([0.25, 0.25, 0.25, 0.25])
+   walker.set_strategy(walk)
+   weights_lst = [
+      [0.75, 0.15, 0.09, 0.01],
+      [0.01, 0.75, 0.15, 0.09],
+      [0.09, 0.01, 0.75, 0.15],
+      [0.15, 0.09, 0.01, 0.75],
+   ]
 
-   while is_in_bounds(walker):
+   iterations = 0
+   while is_in_bounds(walker) and iterations < 100000:
+      divisor = iterations % 4
+      weights = [divisor / 4, divisor / 4, divisor / 4, divisor / 4] 
+      stride = iterations % 8
+      walk.set_weights(weights)
+      # walker.set_stride(stride)
       walker.walk()
       walker.draw()
+      iterations += 1
    
    canvas.write("test")
 
--- random_walk.py
+++ random_walk.py
@@ -2,15 +2,21 @@ import random
 from abc import abstractmethod, ABC
 
 class Walker:
-    def __init__(self, canvas):
+    def __init__(self, canvas, size):
         self.canvas = canvas
+        self.size = size
         self.x = canvas.width / 2
         self.y = canvas.height / 2
+        self.stride = 0
         self.walk_strategy = None
 
     def center(self):
         self.x = self.canvas.width / 2
         self.y = self.canvas.height / 2
+
+    def set_stride(self, stride):
+        assert(stride >= 0)
+        self.stride = stride
 
     def walk(self):
         self.walk_strategy.walk() 
@@ -22,7 +28,7 @@ class Walker:
         context = self.canvas.context
         style = self.canvas.style
         context.circle((self.x, self.y),
-                       1,
+                       self.size,
                        fill=style["fg"],
                        width=style["stroke_width"])
 
@@ -35,29 +41,24 @@ class Walk_Strategy(ABC):
 class Random_Walk(Walk_Strategy):
     def __init__(self, walker):
         self.walker = walker
-        self.stride_length = None
 
     def walk(self):
-        assert(self.stride_length != None)
-
         choice = random.randint(0, 3)
 
         match choice:
             case 0:
                 print("left")
-                self.walker.x += self.stride_length
+                self.walker.x += self.walker.stride
             case 1:
                 print("right")
-                self.walker.x -= self.stride_length
+                self.walker.x -= self.walker.stride
             case 2:
                 print("up")
-                self.walker.y += self.stride_length
+                self.walker.y += self.walker.stride
             case 3:
                 print("down")
-                self.walker.y -= self.stride_length
+                self.walker.y -= self.walker.stride
 
-    def set_stride(self, stride_length):
-        self.stride_length = stride_length
 
 class Weighted_Walk(Walk_Strategy):
     def __init__(self, walker):
@@ -71,21 +72,20 @@ class Weighted_Walk(Walk_Strategy):
 
         if choice < self.weights[0]:
             print("up")
-            self.walker.y -= 1
+            self.walker.y -= self.walker.stride 
         elif choice < sum(self.weights[0:2]):
             print("right")
-            self.walker.x += 1
+            self.walker.x += self.walker.stride
         elif choice < sum(self.weights[0:3]):
             print("down")
-            self.walker.y += 1
+            self.walker.y += self.walker.stride
         else:
             print("left")
-            self.walker.x -= 1
+            self.walker.x -= self.walker.stride
 
     # order of weights starts with up and goes
     # clockwise (up, left, down, right)
     def set_weights(self, weights):
-        assert(sum(weights) == 1)
         assert(len(weights) == 4)
 
         self.weights = weights
